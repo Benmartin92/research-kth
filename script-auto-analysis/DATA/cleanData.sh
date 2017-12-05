cb28d3ae407aeaa2b7904baf0cc38b812174a57f
Marine Collery <Marine.Collery@phelma.grenoble-inp.fr>
2017-12-05 22:48:09 +0100

    Add of a cleaning script

---
+++ script-auto-analysis/DATA/cleanData.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+rm ./coloration-*.txt
+rm ./matrix-*.txt
+rm ./listColorToVFinal-*.txt
+
+rm ./wrong/coloration-*.txt
+rm ./wrong/matrix-*.txt
+rm ./wrong/listColorToVFinal-*.txt
+
